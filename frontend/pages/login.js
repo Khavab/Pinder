c2065d2384dcdb883c190ab05691570e0584846f
Khavab <2021snomani@tjhsst.edu>
2022-04-09 15:31:19 -0400

    setting up linking

---
+++ frontend/pages/login.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Login() {
+    return (
+        <div>
+            <Head>
+                <title>Pinder</title>
+                <meta name="description" content="A website by Shibli Nomani" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    )
+}
