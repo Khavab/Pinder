6677b9755843d1e76cfa2350de85d681081813bb
Khavab <2021snomani@tjhsst.edu>
2022-04-09 16:27:45 -0400

    implementing data use in page

--- frontend/pages/search.js
+++ frontend/pages/search.js
@@ -1,17 +1,31 @@
 import HeadLayout from "../components/headLayout";
+import { getSortedPartyData} from "../lib/parties"
 
-export default function Search(props) {
+export default function Search({ partData}) {
     return (
         <HeadLayout>
-            
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
         </HeadLayout>
     )
 }
 
 export async function getStaticProps() {
-    //const data = 
+    const data = getSortedPartyData
 
-    // return {
-    //     props:
-    // }
+    return {
+        props: partData
+    }
 }
