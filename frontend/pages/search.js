9b652c4a769d9db605647c62f6f515c080c99a82
Khavab <2021snomani@tjhsst.edu>
2022-04-09 16:47:27 -0400

    removed initial data pull, install axios

--- frontend/pages/search.js
+++ frontend/pages/search.js
@@ -1,31 +1,9 @@
 import HeadLayout from "../components/headLayout";
-import { getSortedPartyData} from "../lib/parties"
 
-export default function Search({ partData}) {
+export default function Search() {
     return (
         <HeadLayout>
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
+            
         </HeadLayout>
     )
-}
-
-export async function getStaticProps() {
-    const data = getSortedPartyData
-
-    return {
-        props: partData
-    }
 }
