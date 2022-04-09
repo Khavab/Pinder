d77983c15375f454e497e35af3b960814ec01ac2
Khavab <2021snomani@tjhsst.edu>
2022-04-09 17:13:54 -0400

    data reading from json

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import HeadLayout from '../components/headLayout'
 
 export default function Login() {
--- frontend/pages/search.js
+++ frontend/pages/search.js
@@ -1,9 +1,30 @@
+import React, { useEffect, useState} from "react";
 import HeadLayout from "../components/headLayout";
+import axios from "axios";
 
 export default function Search() {
+    const [data, getData] = useState('');
+
+    useEffect( () => {
+        getAllData();
+      }, []);
+      
+      const getAllData = () => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          const persons = res.data;
+          getData(persons);
+          console.log(persons)
+        })
+      .catch(error => console.error(`Error: ${error}`));
+      }
+
     return (
-        <HeadLayout>
-            
-        </HeadLayout>
+        <div>
+        <HeadLayout></HeadLayout>
+
+        <a>{JSON.stringify(data)}</a>
+
+        </div>
     )
 }
