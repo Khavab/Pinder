8d64c097e955787590289d360f57b9ed3896064c
Khavab <2021snomani@tjhsst.edu>
2022-04-10 11:17:13 -0400

    updated login page

--- frontend/pages/login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import HeadLayout from '../components/headLayout'
-
-export default function Login() {
-    return (
-        <HeadLayout>
-        </HeadLayout>
-    )
-}
